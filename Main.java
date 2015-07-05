7a38052eb850725b63cb568978166c72c63ca1b2
SPiKE-201 <wataru@wataru-no-MacBook-Air.local>
2015-07-05 22:29:58 +0900

    Mainクラスの実装

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.github.finder;
+
+public class Main{
+    public Main(String[] arguments){
+	Args args = parseArguments(arguments);
+	
+        Finder finder = new Finder(args);
+        for(String base: args){
+            String[] items = finder.find(base);
+            for(String item: items){
+                System.out.println(item);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        new Main(args);
+    }
+
+    private Args parseArguments(String[] arguments){
+        Args args = new Args();
+        try {
+            CmdLineParser parser = new CmdLineParser(args);
+            parser.parseArgument(arguments);
+        } catch (CmdLineException e) {
+        }
+        return args;
+    }
+}
